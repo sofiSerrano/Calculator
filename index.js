b3941cd4ecc54eae229ef6caa8287f2bde26d2d1
soffia499 <sofiaserrano499@gmail.com>
2022-02-07 02:25:12 -0300

    the function calculator is done

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 function calcNumbers(result)
 {
-    result = document.querySelector('.area').value + result;
+    document.querySelector('.area').value += result;
 }
-
-function percentage()
+function percentage(result)
+{
+    let result1 = String(result);
+    if(result1.includes("%"))
+    {
+        let a, b , percent;
+        let splitWord = [];
+        a = Number(splitWord[0]);
+        b = Number(splitWord[1]);
+        percent = (a/100 * b);
+        document.querySelector('.area').value = percent;
+    }
+}
+function makeNegative(result)
+{
+    if(result.charAt(0)=='_')
+    {
+        document.querySelector('.area').value = result.replace('-','');
+    }
+    else
+    {
+        document.querySelector('.area').value = '-' + result;
+    }
+}
